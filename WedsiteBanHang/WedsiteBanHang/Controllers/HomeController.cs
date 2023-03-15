b156fe415c6a7817d3d47fc7c1e426e820290ea0
conkunhua <renlotlong@gmail.com>
2023-03-16 01:11:14 +0700

    first commit

---
+++ WedsiteBanHang/WedsiteBanHang/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WedsiteBanHang.Context;
+ 
+using WedsiteBanHang.Models;
+
+namespace WedsiteBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        WedBanHangEntities objWedBanHangEntities = new WedBanHangEntities();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel=new HomeModel();
+            objHomeModel.ListCategory = objWedBanHangEntities.Categories.ToList();
+
+            objHomeModel.ListProduct = objWedBanHangEntities.Products.ToList();
+            return View(objHomeModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
